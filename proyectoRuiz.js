ee29898382c745447e5b491a7b6760c3b79fe835
Victor Ruiz <ruizevictor@gmail.com>
2021-11-10 17:59:24 -0300

    jquery

--- proyectoRuiz.js
+++ proyectoRuiz.js
@@ -1,26 +1,21 @@
-var total = 0;
-var precio = 0;
-var cantidad = 0;
 
 /* Clases */ 
 class Pelicula {
-    constructor(titulo, estreno, duracion, id, precio, horarios, cantidad) {
+    constructor(titulo, estreno, duracion, id, precio, cantidad) {
         this.titulo = titulo;
         this.estreno = estreno;
         this.duracion = duracion;
         this.id = id;
         this.precio = precio;
-        this.horarios = horarios;
         this.cantidad = cantidad
     }
 
     descripcion(){
-        alert(`Se speliculao la pelicula ${this.titulo} estrenada en ${this.estreno}, con una duracion de ${this.duracion} minutos \nCon un precio de ${this.precio} pesos por entrada`);
+        alert(`Se selecciono la pelicula ${this.titulo} estrenada en ${this.estreno}, con una duracion de ${this.duracion} minutos \nCon un precio de ${this.precio} pesos por entrada`);
     }
 
 
 }
-
 
 const pelicula1 = new Pelicula("Kimetsu no Yaiba: Mugen Ressha-hen",2020,117,1,600);
 const pelicula2 = new Pelicula("Akira", 1988, 124, 2, 700);
@@ -29,21 +24,6 @@ const pelicula3 = new Pelicula("El viaje de Chihiro", 2001, 120, 3, 800);
 const catalogo = [pelicula1, pelicula2, pelicula3];
 
 /*Funciones */
-
-
-function ordenCompra(pelicula) {
-    let peliculaComprada = localStorage.getItem('cantidadCompra');
-    peliculaComprada = parseInt(peliculaComprada);
-
-    if(peliculaComprada){
-        localStorage.setItem('cantidadCompra',peliculaComprada+1);
-    } else{
-        localStorage.setItem('cantidadCompra',1);
-
-    }
-
-    elegirPelicula(pelicula);
-}
 
 function elegirPelicula(pelicula){
     let carroProductos = localStorage.getItem('peliculasEnCarro');
@@ -88,67 +68,60 @@ function mostrarCompra(){
     let carroProductos = localStorage.getItem("peliculasEnCarro");
     carroProductos = JSON.parse(carroProductos);
     let costoTotal = localStorage.getItem('costoTotal');
-    let cart = document.getElementsByClassName("peliculasCompradas")[0];
     if(carroProductos){
-        cart.innerHTML = '';
+        $(".peliculasCompradas").html('')
         Object.values(carroProductos).map(item =>{
-            cart.innerHTML += `
-                <div class="peliculas col-3">
+            $(".peliculasCompradas").append(
+                `<div class="peliculas col-3">
                     <p>${item.titulo}</p>
                 </div>
                 <div class="price col-3">${item.precio}</div>
                 <div class="cantidad col-3">${item.cantidad}</div>
-                <div class="total col-3">${item.cantidad * item.precio}</div>`;
+                <div class="total col-3">${item.cantidad * item.precio}</div>`)
+
         });
 
-        cart.innerHTML += `
-        <div class="totalPagar col-9">
-                    <p>Total</p>
-                </div>
-                <div class="priceTotal col-3">${costoTotal}</div>
-        `
+        $(".peliculasCompradas").append(
+            `<div class="totalPagar col-9">
+                <p>Total</p>
+            </div>
+            <div class="priceTotal col-3">${costoTotal}</div>`)
 
     }
 }
 
 mostrarCompra()
 
-
-
-
-
-
+function eliminarCarrito(){
+    localStorage.setItem("peliculasEnCarro",null);
+    localStorage.setItem('costoTotal',"0");
+    $(".peliculasCompradas").html('');
+}
 
 
 /* Eventos*/
 
-let mostrarCatalogo = document.getElementById("mostrarCatalogo")
-let catalogoClick = document.getElementsByClassName("btn")[0];
-catalogoClick.addEventListener("click", () => {
-    mostrarCatalogo.innerHTML=``;
-    for (const pelicula of catalogo) {
-        let contenedor = document.createElement("div");
-        contenedor.innerHTML = `<div class="col-md-3 col-4">
-                                    <h3 class="card">${pelicula.id}-${pelicula.titulo}</h3>
-                                    <p class="card"> Precio: ${pelicula.precio} $ </p>
-                                </div>
-                                `;
-    
-        mostrarCatalogo.appendChild(contenedor);
-
-    }
-
-})
+for (const pelicula of catalogo) {
+    $(".pelicula").append(`
+        <div class=" col-5 col-md-4 col-lg-3 m-lg-1">
+            <div class="card">
+                <img src="images/pelicula${pelicula.id}.webp" class="card-img pelicula${pelicula.id}" alt="${pelicula.titulo}">
+            </div>
+        </div>`
+    )}
 
 
-let compraClick = document.querySelectorAll(".card-img");
 
-for (let i = 0; i<compraClick.length; i++) {
-    compraClick[i].addEventListener("click", () => {
-        ordenCompra(catalogo[i]);
+for (let i = 0; i<catalogo.length; i++) {
+    $(`.pelicula${(i+1)}`).on("click", () => {
+        elegirPelicula(catalogo[i]);
         precioOrden(catalogo[i]);
         mostrarCompra();
     })
 
 }
+
+$(".btnLimpiar").on("click", () => {
+    eliminarCarrito();
+})
 
